270e646f1eb35218d01e6d394dfdb6d495c24630
Lydia Simmons <saybaar@gmail.com>
2022-02-02 20:17:19 -0500

    fix url prefix for Github Pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,17 @@
 const { DateTime } = require("luxon");
 
-
-
 module.exports = function (eleventyConfig) {
+
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
+
     return {
         dir: {
             input: "src",
             output: "_site"
         },
-        htmlTemplateEngine: "njk"
+        htmlTemplateEngine: "njk",
+        pathPrefix: "/haven-site/"
     }
 };
